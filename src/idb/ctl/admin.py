cad189b6f9f988bd6e8c2c4c87b30fbddc5fa18e
Mathieu Lacage <mathieu.lacage@cutebugs.net>
2025-12-21 18:40:56 +0100

    initialize works

--- src/idb/ctl/admin.py
+++ src/idb/ctl/admin.py
@@ -1,12 +1,16 @@
 import requests
 
 from . import config
+from . import exceptions
 
 def _initialize_function(args):
     c = config.Config.load(args.config)
     response = requests.post(c.directory['initialize'])
-    print(response.text, response.status_code)
+    if response.status_code == 204:
+        raise exceptions.UI(f'Unable to initialize app: it is already initialized.')
     response.raise_for_status()
+    data = response.json()
+    print(f'{data["key_id"]}:{data["key"]}')
 
 
 def add_subparsers(parser):
