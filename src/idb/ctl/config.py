c6743705f0d0b4dd5c8173df1c282074f8cc992f
Mathieu Lacage <mathieu.lacage@cutebugs.net>
2025-12-23 07:26:56 +0100

    missing files

---
+++ src/idb/ctl/config.py
@@ -0,0 +1,25 @@
+import dataclasses
+import json
+
+
+@dataclasses.dataclass
+class Config:
+    directory_url: str
+    root_key_id: str
+    ignore_ssh_agent: bool
+    directory: dict = None
+
+    @staticmethod
+    def load(filename):
+        with open(filename) as f:
+            data = json.load(f)
+            return Config(**data)
+
+    def save(self, filename):
+        with open(filename, 'w+') as f:
+            json.dump(dict(
+                directory_url=self.directory_url,
+                root_key_id=self.root_key_id,
+                ignore_ssh_agent=self.ignore_ssh_agent,
+                directory=self.directory,
+            ), f)
