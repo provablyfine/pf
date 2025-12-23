f6102062097cedff8a6a7259afb48eee6d6a05e1
Mathieu Lacage <mathieu.lacage@cutebugs.net>
2025-12-23 20:31:53 +0100

    dead code

--- src/idb/wa/middleware.py
+++ src/idb/wa/middleware.py
@@ -10,20 +10,3 @@ class Middleware:
 
     def __call__(self, request: Request, iterator: collections.abc.Iterator[Middleware]) -> Response:
         raise NotImplementedError
-
-
-class LastMiddleware:
-    """
-    An internal middleware used by the main application to hold
-    the endpoint handler that will be processed at the bottom
-    of the middleware stack
-    """
-
-    def __init__(self, handler):
-        self._handler = handler
-
-    def __call__(self, request: Request, iterator: collections.abc.Iterator[Middleware]) -> Response:
-        next_middleware = next(iterator, None)
-        if next_middleware is not None:
-            return ProblemResponse(status_code=500, title='Last middleware should be last')
-        return self._handler(request)
