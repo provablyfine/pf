263e41d167d5101720e3e53f9e54cbc6e6049cff
Mathieu Lacage <mathieu.lacage@cutebugs.net>
2026-02-02 18:59:55 +0100

    code has been moved

--- src/idb/cli/ssh_cli.py
+++ src/idb/cli/ssh_cli.py
@@ -2,6 +2,8 @@ import tabulate
 
 from . import exceptions
 from . import ssh_utils
+from . import ssh_agent_cli
+from . import ssh_cert_cli
 from .. import ssh
 from .. import jwk
 
@@ -24,67 +26,6 @@ def _download_user_signing_key_function(args):
 
 def _list_remotes_function(args):
     pass
-
-@ssh_utils.exception
-def _agent_list_identities_function(args):
-    ssh_agent = ssh.agent.Client()
-    rows = []
-    for id in ssh_agent.list_identities():
-        key = jwk.Public.from_ssh_bytes(id.public_key)
-        rows.append((key.ssh_fingerprint(), str(key.type)[len('KeyType.'):], id.comment))
-        serialized = key.to_ssh_bytes()
-        assert serialized == id.public_key
-    if len(rows) > 0:
-        print(tabulate.tabulate(rows, headers=['fingerprint', 'key type', 'comment']))
-
-
-def _agent_find_by_fingerprint(ssh_agent, fingerprint):
-    for identity in ssh_agent.list_identities():
-        key = jwk.Public.from_ssh_bytes(identity.public_key)
-        if key.match_ssh_fingerprint(fingerprint):
-            return identity
-    raise exceptions.UI(f'Unable to find key {fingerprint}')
-
-
-@ssh_utils.exception
-def _agent_sign_function(args):
-    ssh_agent = ssh.agent.Client()
-    identity = _agent_find_by_fingerprint(ssh_agent, args.fingerprint)
-    with open(args.data, 'rb') as f:
-        data = f.read()
-    match args.flags:
-        case None:
-            flags = 0
-        case 'SHA2_256':
-            flags = ssh.agent.RSA.SHA2_256
-        case 'SHA2_512':
-            flags = ssh.agent.RSA.SHA2_512
-        case _:
-            assert False
-    signature = ssh_agent.sign(identity.public_key, data, flags=flags)
-    print(signature)
-
-
-@ssh_utils.exception
-def _agent_add_function(args):
-    ssh_agent = ssh.agent.Client()
-    with open(args.filename, 'rb') as f:
-        data = f.read()
-    key = jwk.Private.from_ssh(data)
-    ssh_agent.add(key.to_ssh_bytes(), comment=args.comment, lifetime=args.lifetime, require_confirmation=args.require_confirmation)
-
-
-@ssh_utils.exception
-def _agent_del_function(args):
-    ssh_agent = ssh.agent.Client()
-    if args.all:
-        ssh_agent.remove_all()
-    elif args.fingerprint:
-        identity = _agent_find_by_fingerprint(ssh, args.fingerprint)
-        ssh_agent.remove(identity.public_key)
-    else:
-        assert False
-
 
 def add_subparsers(parser):
     parser.add_argument('--session-key', help='key to use to sign requests to the remote', default='session.key')
@@ -110,6 +51,12 @@ def add_subparsers(parser):
 
     list_remotes_parser = subparsers.add_parser('list-remotes', help='List all remotes the current identity is allowed to connect to')
     list_remotes_parser.set_defaults(func=_list_remotes_function)
+
+    agent_parser = subparsers.add_parser('agent', help='ssh agent')
+    ssh_agent_cli.add_subparsers(agent_parser)
+
+    cert_parser = subparsers.add_parser('cert', help='certificates')
+    ssh_cert_cli.add_subparsers(cert_parser)
 
     #login_parser = subparsers.add_argument('login')
     #login_parser.set_defaults(func=_login_function)
