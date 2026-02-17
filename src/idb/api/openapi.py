9793c41baf564249cf66bb798c91a67b8c16614f
Mathieu Lacage <mathieu.lacage@cutebugs.net>
2026-02-17 22:20:45 +0100

    dead code

--- src/idb/api/openapi.py
+++ src/idb/api/openapi.py
@@ -4,8 +4,6 @@ import os.path
 import urllib
 
 import yaml
-import starlette
-import openapi_core.contrib.starlette.middlewares
 import openapi_core
 
 from .. import wa
@@ -18,22 +16,6 @@ logger = logging.getLogger(__name__)
 # We implement our own middleware that is based on the
 # openapi_core contrib middleware to control the format
 # of error responses
-
-class ErrorsHandler(openapi_core.contrib.starlette.middlewares.StarletteOpenAPIErrorsHandler):
-    def __call__(
-        self,
-        errors: typing.Iterable[Exception],
-    ) -> starlette.responses.JSONResponse:
-        causes = [error if error.__cause__ is None else error.__cause__ for error in errors]
-        status_code = max([self.OPENAPI_ERROR_STATUS.get(error.__class__, 400) for error in causes])
-        body = {
-            'type': 'about:blank',
-            'title': 'Malformed content',
-            'detail': ', '.join([str(error) for error in causes]),
-            'status': status_code
-        }
-        return starlette.responses.JSONResponse(body, status_code=status_code)
-
 
 def _load_specification(base_url):
     filename = os.path.join(os.path.dirname(__file__), 'openapi.yaml')
