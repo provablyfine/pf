dab22fc35515277191b2664f52a7056734fe8b09
Mathieu Lacage <mathieu.lacage@cutebugs.net>
2026-04-09 21:13:43 +0200

    type hints

--- src/pf/api/crypto_policy.py
+++ src/pf/api/crypto_policy.py
@@ -2,7 +2,7 @@ from .. import jwk
 from . import responses
 
 
-def enforce_key_is_allowed(key):
+def enforce_key_is_allowed(key: jwk.Public) -> None:
     if key.type != jwk.KeyType.ED25519:
         raise responses.ProblemHTTPException(
             responses.problem_response(status_code=403, title="Key type not supported", detail=str(key.type))
