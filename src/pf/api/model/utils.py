57b5e0403a386a4f3a5899cbe9b46497c987d59a
Mathieu Lacage <mathieu.lacage@cutebugs.net>
2026-04-11 20:01:14 +0200

    make group_by type-safe

--- src/pf/api/model/utils.py
+++ src/pf/api/model/utils.py
@@ -5,12 +5,11 @@ import typing
 class Sortable(typing.Protocol):
     def __lt__(self, other: typing.Any) -> bool: ...
 
-type GroupedResult[K, T] = list[tuple[K, list[T]]]
 
 def group_by[T, K: Sortable](
     seq: typing.Iterable[T],
     key: typing.Callable[[T], K]
-) -> GroupedResult[K, T]:
+) -> list[tuple[K, list[T]]]:
 
     sorted_list: list[T] = sorted(seq, key=key)
     groups = itertools.groupby(sorted_list, key=key)
