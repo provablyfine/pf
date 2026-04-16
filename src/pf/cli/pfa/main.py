a149fc700bb71d0eba46604263f0ebf7a68b87d5
Mathieu Lacage <mathieu.lacage@cutebugs.net>
2026-04-16 09:49:53 +0200

    avoid using private type

--- src/pf/cli/pfa/main.py
+++ src/pf/cli/pfa/main.py
@@ -9,7 +9,7 @@ import requests
 
 from ... import client, log
 from .. import login
-from . import admin_cli
+from . import auth_cli, bastion_cli, boundary_cli, grant_cli, identity_cli, role_cli, tag_cli, tenant_cli
 
 _DEFAULT_CONFIG = os.path.join(os.path.expanduser("~"), ".config", "pf", "config.json")
 
@@ -134,7 +134,29 @@ def pfa():
     )
     login_parser.set_defaults(func=_login_function)
 
-    admin_cli.add_subparsers(subparsers)
+    boundary_parser = subparsers.add_parser("boundary", help="View and edit boundaries")
+    boundary_cli.add_subparser(boundary_parser)
+
+    tag_parser = subparsers.add_parser("tag", help="View and edit tags")
+    tag_cli.add_subparser(tag_parser)
+
+    role_parser = subparsers.add_parser("role", help="View and edit roles")
+    role_cli.add_subparser(role_parser)
+
+    identity_parser = subparsers.add_parser("identity", help="View and edit identities")
+    identity_cli.add_subparser(identity_parser)
+
+    grant_parser = subparsers.add_parser("grant", help="Generate grants")
+    grant_cli.add_subparser(grant_parser)
+
+    tenant_parser = subparsers.add_parser("tenant", help="View and manage tenants")
+    tenant_cli.add_subparser(tenant_parser)
+
+    auth_parser = subparsers.add_parser("auth", help="Manage authentication configurations")
+    auth_cli.add_subparser(auth_parser)
+
+    bastion_parser = subparsers.add_parser("bastion", help="View and edit bastions")
+    bastion_cli.add_subparser(bastion_parser)
 
     args = parser.parse_args()
 
