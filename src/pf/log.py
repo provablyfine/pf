4043154667238deaeaa21c11506c5fdde1c403f5
Mathieu Lacage <mathieu.lacage@cutebugs.net>
2026-05-09 15:12:33 +0200

    generic trace function

--- src/pf/log.py
+++ src/pf/log.py
@@ -1,8 +1,70 @@
+from __future__ import annotations
+
+import functools
+import inspect
 import logging
 import os
+import sys
+import types
+import typing
 
 FORMAT = "%(asctime)s:%(levelname)s:%(module)s.%(funcName)s:%(message)s"
 DATEFMT = "%H:%M:%S"
+
+TraceFunc = typing.Callable[[types.FrameType, str, typing.Any], "TraceFunc | None"]
+
+logger = logging.getLogger(__name__)
+
+
+def trace_lines(func: typing.Callable[..., typing.Any]) -> typing.Callable[..., typing.Any]:
+    """
+    Enable debug logging for each single line in a function.
+    Functions called by this function are not traced.
+    """
+
+    def get_tracer(initial_frame: types.FrameType) -> TraceFunc:
+        def tracer(frame: types.FrameType, event: str, arg: typing.Any) -> TraceFunc | None:
+            if frame is not initial_frame:
+                return None
+            if event == "line":
+                module_name = frame.f_globals.get("__name__", "<unknown>")
+                qualname = getattr(frame.f_code, "co_qualname", frame.f_code.co_name)
+                logger.debug(f"Trace {module_name}.{qualname}:{frame.f_lineno}")
+            return tracer
+
+        return tracer
+
+    def get_initial_tracer() -> TraceFunc:
+        # We need an initial_tracer to capture the frame of the function
+        activated = False
+
+        def initial_tracer(frame: types.FrameType, event: str, arg: typing.Any) -> TraceFunc | None:
+            nonlocal activated
+            if event == "call" and not activated:
+                activated = True
+                return get_tracer(frame)
+            return None
+
+        return initial_tracer
+
+    @functools.wraps(func)
+    async def async_wrapper(*args: typing.Any, **kwargs: typing.Any) -> typing.Any:
+        sys.settrace(get_initial_tracer())
+        try:
+            return await func(*args, **kwargs)
+        finally:
+            sys.settrace(None)
+
+    @functools.wraps(func)
+    def sync_wrapper(*args: typing.Any, **kwargs: typing.Any) -> typing.Any:
+        sys.settrace(get_initial_tracer())
+        try:
+            return func(*args, **kwargs)
+        finally:
+            sys.settrace(None)
+
+    # switch implementation for async vs normal functions
+    return async_wrapper if inspect.iscoroutinefunction(func) else sync_wrapper
 
 
 def setup(debug: int, log_filename: str) -> None:
