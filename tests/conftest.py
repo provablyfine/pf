af11df68e1b88208ee15a1229c77542d8d6987ec
Mathieu Lacage <mathieu.lacage@cutebugs.net>
2026-05-23 09:59:14 +0200

    FUCK YOU ruff

--- tests/conftest.py
+++ tests/conftest.py
@@ -159,9 +159,7 @@ CMD ["/bin/sh", "/run/start.sh"]
         container_file.write(containerfile)
         container_file.flush()
 
-        stdout = _run(
-            ["podman", "build", "--quiet", "--file", container_file.name, tld()], tmp_path
-        )
+        stdout = _run(["podman", "build", "--quiet", "--file", container_file.name, tld()], tmp_path)
         image_id = stdout.strip("\n")
         if "\n" in image_id:
             assert False, image_id
@@ -232,9 +230,7 @@ CMD ["/sbin/init"]
     with tempfile.NamedTemporaryFile(mode="w+", suffix=".Containerfile", dir=tmp_path, delete=False) as container_file:
         container_file.write(containerfile)
         container_file.flush()
-        stdout = _run(
-            ["podman", "build", "--quiet", "--file", container_file.name, tld()], tmp_path
-        )
+        stdout = _run(["podman", "build", "--quiet", "--file", container_file.name, tld()], tmp_path)
         image_id = stdout.strip("\n")
         if "\n" in image_id:
             assert False, image_id
